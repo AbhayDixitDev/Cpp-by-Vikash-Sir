c99361825f95a8c11b8548b3bc836a6e42e34150
Abhay Dixit <dixitshivam534@gmail.com>
2024-11-27 22:33:40 +0530

    logic change

--- swap composite class.cpp
+++ swap composite class.cpp
@@ -1,37 +1,71 @@
+//#include <iostream>
+//using namespace std;
+//
+//
+//
+//class swap1 {
+//public:
+//    
+//    void swap2(int a,int b) {
+//        int temp = a;
+//        a = b;
+//        b = temp;
+//        cout << "After swap:" << endl;
+//        cout << "a = " << a << endl;
+//        cout << "b = " << b << endl;
+//    }
+//};
+//
+//class input {
+//public:
+//    int a;
+//    int b;
+//    swap(int k,int l)
+//    {
+//    	a=k;
+//    	b=l;
+//	}
+//	swap1 obj;
+//	obj.swap2(a,b);
+//	
+//};
+//
+//int main() {
+//    input h;
+//    h.swap(10,20);
+//    
+//}
+
+
+
 #include <iostream>
 using namespace std;
+
+class swap1 {
+public:
+    void swap2(int a, int b) { 
+        int temp = a;
+        a = b;
+        b = temp;
+        cout << "After swap:" << endl;
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+    }
+};
 
 class input {
 public:
     int a;
-    int b;
-    void num(int a1, int b1) {
-        a = a1;
-        b = b1;
-    }
-};
-
-class swap1 {
-public:
-    input h; 
-    void swap() {
-        int temp = h.a;
-        h.a = h.b;
-        h.b = temp;
-
-        cout << "After swap:" << endl;
-        cout << "a = " << h.a << endl;
-        cout << "b = " << h.b << endl;
+    int b;   
+    void performSwap(int s,int t) { 
+        a=s;
+        b=t;
+		swap1 obj;
+        obj.swap2(a, b); 
     }
 };
 
 int main() {
     input h;
-    h.num(10, 20); 
-
-    swap1 s; 
-    s.h = h; 
-    s.swap();
-
-    return 0;
+    h.performSwap(10,20);
 }
